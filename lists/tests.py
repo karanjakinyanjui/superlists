0d5ea0ed1017619b3dab2c51b32446146ce9b6b8
Elijah <ekkinyanjui@gmail.com>
2022-08-11 17:23:20 +0300

    Added a URL for creating a new list via POST

--- lists/tests.py
+++ lists/tests.py
@@ -19,14 +19,27 @@ class ListViewTest(TestCase):
         Item.objects.create(text='The first list item')
         Item.objects.create(text='The second list item')
 
-        response = self.client.get('/lists/the-only-list-in-the-world')
+        response = self.client.get('/lists/the-only-list-in-the-world/')
 
         self.assertContains(response, 'The first list item')
         self.assertContains(response, 'The second list item')
 
     def test_uses_list_template(self):
-        response = self.client.get('/lists/the-only-list-in-the-world')
+        response = self.client.get('/lists/the-only-list-in-the-world/')
         self.assertTemplateUsed(response, 'lists/list.html')
+
+
+class NewListTest(TestCase):
+    def test_can_save_a_POST_request(self):
+        todo_text = fake.sentence()
+        self.client.post('/lists/new', data={"text": todo_text})
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, todo_text)
+
+    def test_redirects_after_POST(self):
+        todo_text = fake.sentence()
+        response = self.client.post('/lists/new', data={"text": todo_text})
+        self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
 
 
 class HomePageTest(TestCase):
@@ -40,15 +53,3 @@ class HomePageTest(TestCase):
         self.assertTemplateUsed(response, 'lists/home.html')
         html = response.content.decode('utf-8').strip()
         self.assertIn('<title>To-Do lists</title>', html)
-
-    def test_can_save_a_POST_request(self):
-        todo_text = fake.sentence()
-        self.client.post('/', data={"text": todo_text})
-        new_item = Item.objects.first()
-        self.assertEqual(new_item.text, todo_text)
-
-    def test_redirects_after_POST_request(self):
-        todo_text = fake.sentence()
-        response = self.client.post('/', data={"text": todo_text})
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world')
