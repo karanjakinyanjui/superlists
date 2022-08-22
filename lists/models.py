d32517398cabf55b3e13200fea01b45433e5d905
Elijah <ekkinyanjui@gmail.com>
2022-08-22 14:08:45 +0300

    Before branch

--- lists/models.py
+++ lists/models.py
@@ -7,14 +7,13 @@ class List(models.Model):
         return reverse('view_list', args=[self.id])
 
 
-
 class Item(models.Model):
     text = models.TextField(default='')
     list = models.ForeignKey(List, default=None, on_delete=models.CASCADE)
 
     class Meta:
         ordering = ('id',)
-        unique_together = (('text', 'list'), )
+        unique_together = (('text', 'list'),)
 
     def __str__(self):
         return self.text
